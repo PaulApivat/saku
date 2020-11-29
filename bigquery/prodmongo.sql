3f1a8efd7254360966f1b8cd963043abe3f1f891
paulapivat <paul.apivat@gmail.com>
2020-11-29 16:14:12 +0700

    add query for temp_jobs and temp_profiles

---
+++ bigquery/prodmongo.sql
@@ -0,0 +1,8 @@
+SELECT position, job_description, created_at 
+FROM `jobsbot-276604.prodmongo.jobs` 
+LIMIT 10
+
+
+SELECT full_name, age, gender, university_name, created_at 
+FROM `jobsbot-276604.prodmongo.profiles` 
+LIMIT 10
