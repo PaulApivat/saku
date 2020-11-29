e29e9eca5e84b95313b3f44f47bfdb0c09a1a661
paulapivat <paul.apivat@gmail.com>
2020-11-29 16:15:24 +0700

    add comments and source details

--- bigquery/prodmongo.sql
+++ bigquery/prodmongo.sql
@@ -1,8 +1,11 @@
+# source: BigQuery > jobsbot > prodmongo
+
+# temp_jobs
 SELECT position, job_description, created_at 
 FROM `jobsbot-276604.prodmongo.jobs` 
 LIMIT 10
 
-
+# temp_profiles
 SELECT full_name, age, gender, university_name, created_at 
 FROM `jobsbot-276604.prodmongo.profiles` 
 LIMIT 10
